df7e78db1255d431c99e0daca3af9aa6ea2d728e
Alexander Grissinger <alex.grissinger@hotmail.com>
2019-11-07 15:45:26 +0100

    Added files for question two

---
+++ Exercise 2 Reflected Cross Side Scripting/server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var UserData =[];
+app.get("/", (req, res, next) => {    
+    UserData.push(req.query)
+    console.log(req.query);
+    if (UserData.length >1){
+        res.json("Username: "+ UserData[1].uid+",  Password: "+ UserData[1].passw+", Cookie: "+ UserData[0].AltoroAccounts);
+    }else{
+        res.json("waiting...")
+    }
+    
+   // res.json("hello")
+   });
+
+   app.post("/user", (req, res, next) => {
+       console.log(req);
+    res.json("Welcome");
+   });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
